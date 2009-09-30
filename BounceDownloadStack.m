77c4b3411aef246773597d0b9409b6df7acef899
fppzdd <fppzdd@gmail.com>
2009-09-30 21:51:54 +0200

    Added some more magic, for filenames with spaces in them

--- BounceDownloadStack.m
+++ BounceDownloadStack.m
@@ -3,14 +3,15 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
-	NSArray * args = [[NSProcessInfo processInfo] arguments];
+	NSMutableArray * args = [NSMutableArray arrayWithArray:[[NSProcessInfo processInfo] arguments]];
 	
 	if ([args count] > 1) {
+		[args removeObjectAtIndex:0];
 		[[NSDistributedNotificationCenter defaultCenter]
 		 postNotificationName:@"com.apple.DownloadFileFinished" 
-		 object:[args objectAtIndex: 1]];
+		 object:[args componentsJoinedByString:@" "]];
 	}
-	
+
     [pool drain];
     return 0;
 }
