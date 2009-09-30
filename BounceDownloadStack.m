3d43b038a1c30a906297493e59fd4799db675a7f
fppzdd <fppzdd@gmail.com>
2009-09-30 20:54:05 +0200

    first arg is the file path

--- BounceDownloadStack.m
+++ BounceDownloadStack.m
@@ -1,15 +1,12 @@
 #import <Foundation/Foundation.h>
-#import <Foundation/NSDistributedNotificationCenter.h>
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	
+		
 	[[NSDistributedNotificationCenter defaultCenter]
 		postNotificationName:@"com.apple.DownloadFileFinished" 
-		object:@"/Users/adam/Downloads/asdf"];
+		object:[[[NSProcessInfo processInfo] arguments] objectAtIndex: 1]];
 	
-    // insert code here...
-    NSLog(@"Download complete!");
     [pool drain];
     return 0;
 }
